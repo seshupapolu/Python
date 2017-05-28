26c66c677b79c4cc3181e612e76aa5f2284d21d4
seshu <papolu.cloud@outlook.com>
2017-05-28 13:44:52 +0530

    Added to function

--- Practice/Practice/AccessingWeb.py
+++ Practice/Practice/AccessingWeb.py
@@ -1,9 +1,14 @@
 from urllib.request import urlopen
-with urlopen("http://sixty-north.com/c/t.txt") as story:
-    story_words=[]
-    for line in story:
-        line_word=line.decode('utf-8').split()
-        for word in line_word:
-            story_words.append(word)
 
-print(story_words)
+def fetch_words():
+    with urlopen("http://sixty-north.com/c/t.txt") as story:
+        story_words=[]
+        for line in story:
+            line_word=line.decode('utf-8').split()
+            for word in line_word:
+                story_words.append(word)
+    print(story_words)
+
+print(__name__)
+if(__name__=="__main__"):
+    fetch_words()
